5d22ba71a1d615f334f7e712dd536dfba33ffa2c
miagisellemv07 <miagiselleminjarezv@gmail.com>
2026-04-17 17:31:15 -0700

    iohjoijoñ

--- Dashboard/bootstrap/app.php
+++ Dashboard/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\JwtMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +13,18 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+           $middleware->alias([
+            'jwt' => JwtMiddleware::class
+        ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->render(function (\Illuminate\Validation\ValidationException $e, $request) {
+    if ($request->is('api/*')) {
+        return response()->json([
+            'error' => 'Validación fallida',
+            'message' => 'Los datos enviados no son correctos para los estándares de BitCorp.',
+            'details' => $e->errors(),
+        ], 422);
+    }
+});
     })->create();
--- Dashboard/routes/api.php
+++ Dashboard/routes/api.php
@@ -14,6 +14,17 @@ Route::resource('/rentals',RentalController::class);
 Route::resource('/drivers',driverController::class);
 Route::resource('/cars',CarController::class);
 Route::resource('/brands',BrandController::class);
+use App\Http\Controllers\Api\AuthController;
 Route::resource('/loyalty_levels',loyalty_levelController::class);
 Route::put('rentals/{id}/status', [RentalController::class, 'updateStatus']);
 Route::put('cars/{id}/status', [CarController::class, 'updateStatus']);
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('jwt')->group(function () {
+    Route::get('/user', [AuthController::class, 'getUser']);
+    Route::put('/user', [AuthController::class, 'updateUser']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
